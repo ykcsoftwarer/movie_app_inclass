27dd711f8adad54791a34b7759e52cdb31694cca
ykcsoftwarer <yusufkucukcopur@gmail.com>
2022-11-07 20:50:22 +0300

    start movie app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Register from "./pages/Register";
+import index from './index.css'
 
 const App = () => {
   return (
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "../pages/Main";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import MovieDetail from "../pages/MovieDetail";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/details/:id" element= {<MovieDetail/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default AppRouter;
